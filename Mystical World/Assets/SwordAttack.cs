07725c064c1f53b50fd331f5d53910b24bb2bd92
Pepsi321 <127976380+Pepsi321@users.noreply.github.com>
2023-04-18 20:10:33 +0300

    Add_Sword
    Add_Sword

---
+++ Mystical World/Assets/SwordAttack.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    public Animator sword;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            sword.SetTrigger("Triger");
+        }
+    }
+}
