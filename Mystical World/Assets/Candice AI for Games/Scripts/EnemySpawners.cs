1c674405be0f4c1f6fb98f86f6d6c108b2e24468
LKAYHot <128542643+LKAYHot@users.noreply.github.com>
2023-04-28 21:46:49 +0300

    New Enemy spawner with limit

--- Mystical World/Assets/Candice AI for Games/Scripts/EnemySpawners.cs
+++ Mystical World/Assets/Candice AI for Games/Scripts/EnemySpawners.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +13,9 @@ public class EnemySpawners : MonoBehaviour
     //GameObject[] rootEnemy;
     //parent layer parallax
     private GameObject parentLayer;
+
+    public int MaxAmountEnemies = 5;
+    private int DeleteAmount = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -31,30 +34,37 @@ public class EnemySpawners : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (MaxAmountEnemies > 0)
+        {
         timer += Time.deltaTime;
 
-        if (timer >= interval)
-        {
-            timer = 0f;
-            if (enemyPrefab != null)
+            if (timer >= interval)
             {
-                if (parentLayer != null)
-                {                    
-                    var enemy = Instantiate(enemyPrefab, transform.position, transform.rotation, parentLayer.transform);
-                    var spawn_fx = Instantiate(SpawnFx, new Vector3(transform.position.x - 0.4f, transform.position.y - 0.3f, 1), transform.rotation);
-                    spawn_fx.transform.SetParent(enemy.transform);
-                    Destroy(spawn_fx, 2f);
-                }
-                else {
-                    var enemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
-                    var spawn_fx = Instantiate(SpawnFx, new Vector3(transform.position.x - 0.4f, transform.position.y - 0.3f, 1), transform.rotation);                    
-                    spawn_fx.transform.SetParent(enemy.transform);
-                    Destroy(spawn_fx, 2f);
-                }
+                timer = 0f;
+                if (enemyPrefab != null)
+                {
+                    if (parentLayer != null)
+                    {
+                        MaxAmountEnemies = MaxAmountEnemies - DeleteAmount;
+                        var enemy = Instantiate(enemyPrefab, transform.position, transform.rotation, parentLayer.transform);
+                        var spawn_fx = Instantiate(SpawnFx, new Vector3(transform.position.x - 0.4f, transform.position.y - 0.3f, 1), transform.rotation);
+                        spawn_fx.transform.SetParent(enemy.transform);
+                        Destroy(spawn_fx, 2f);
+                        
+                    }
+                    else
+                    {
+                        MaxAmountEnemies = MaxAmountEnemies - DeleteAmount;
+                        var enemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
+                        var spawn_fx = Instantiate(SpawnFx, new Vector3(transform.position.x - 0.4f, transform.position.y - 0.3f, 1), transform.rotation);
+                        spawn_fx.transform.SetParent(enemy.transform);
+                        Destroy(spawn_fx, 2f);
+                    }
 
+                }
             }
-        }        
+        }
+        Debug.Log(MaxAmountEnemies);
         
     }
 
