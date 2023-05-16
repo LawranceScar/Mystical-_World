d9253f4fd1fb33a8844278aa38e5edd4a8f113fe
Pepsi321 <127976380+Pepsi321@users.noreply.github.com>
2023-05-16 21:30:09 +0300

    Sword

--- Mystical World/Assets/Takedamage.cs
+++ Mystical World/Assets/Takedamage.cs
@@ -7,24 +7,31 @@ public class Takedamage : MonoBehaviour
     public float hp = 50;
     void Start()
     {
-        
+
     }
 
-   
+
     void Update()
     {
 
-       if(hp <= 0)
+        if (hp <= 0)
         {
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log(collision.transform.name);
         if (collision.gameObject.CompareTag("Bullet"))
         {
             BulletScript Myb = collision.gameObject.GetComponent<BulletScript>();
             hp = hp - Myb.Damage;
+
         }
-       
+        if (collision.gameObject.CompareTag("Sword"))
+        {
+
+            Sworddamage myg = collision.gameObject.GetComponent<Sworddamage>();
+            hp = hp - myg.damage;
+        }
     }
 }
