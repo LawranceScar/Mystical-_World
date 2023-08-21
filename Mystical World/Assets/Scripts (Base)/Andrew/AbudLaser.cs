4c5f0a60360235885c38dfb62f52041b529d6b93
Pepsi321 <127976380+Pepsi321@users.noreply.github.com>
2023-08-21 10:04:47 +0300

    Fix Laser
    nuh uh

--- Mystical World/Assets/Scripts (Base)/Andrew/AbudLaser.cs
+++ Mystical World/Assets/Scripts (Base)/Andrew/AbudLaser.cs
@@ -35,7 +35,6 @@ public class AbudLaser : MonoBehaviour
            myshootlaser = TopLaser.GetComponent<ShootLaser>();
             if(myshootlaser != null)
             {
-
                 myshootlaser.ShootLazer();
             }
            
