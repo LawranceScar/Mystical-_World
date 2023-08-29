71d09811bcac0a9ab46a9f0d4e2e0a92c4ed73b9
LawranceScar <aartemmovchan@gmail.com>
2023-08-29 21:39:04 +0200

    Prototype_FallDamage

--- Mystical World/Assets/Scripts (Base)/Artem/FallDamage.cs
+++ Mystical World/Assets/Scripts (Base)/Artem/FallDamage.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class FallDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+    private float Distance;
+    [SerializeField] private GameObject Player;
+
     void Start()
     {
-        
+        rb = Player.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-
+            if (rb.velocity.y > 20)
+            {
+                Distance = rb.velocity.y / 2;
+                                                    //HP -= Distance
+            }
         }
     }
+
 }
