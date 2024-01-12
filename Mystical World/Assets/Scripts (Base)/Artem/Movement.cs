7d98cd4065e5ebf7516137341539bc404ad8fbfd
LawranceScar <aartemmovchan@gmail.com>
2024-01-12 18:59:57 +0100

    Jump_Debug

--- Mystical World/Assets/Scripts (Base)/Artem/Movement.cs
+++ Mystical World/Assets/Scripts (Base)/Artem/Movement.cs
@@ -19,8 +19,8 @@ public class Movement : MonoBehaviour
     private float Jump;
     private float Sprint;
 
-    private float JumpCount = 0;
-    private float MaxJumpCount = 2;
+    public int JumpCount = 0;
+    private int MaxJumpCount = 5;
 
     private bool Dash;
     [SerializeField] private float DashDistance = 2;
@@ -36,10 +36,6 @@ public class Movement : MonoBehaviour
     {
         
         All_Inputs();
-        Debug.Log($"Horizontal = {MoveHorizontal}");
-        Debug.Log($"MoveVertical = {MoveVertical}");
-        Debug.Log($"Jump = {Jump}");
-        Debug.Log($"Dash = {Dash}");
 
         if (Sprint != 0.0f && MoveVertical > 0.0f)
         {
@@ -56,26 +52,17 @@ public class Movement : MonoBehaviour
         Vector3 JumpPower = Jump * gameObject.transform.up;
         Vector3 DashPower = Vector3.zero;
 
-        /*       Vector3 LookForward = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
-           Quaternion PlayerForward = Quaternion.LookRotation(LookForward);
-        Quaternion Rotation = PlayerForward * CameraTransform.rotation;
-        float yVelocity = 0.0f;
-        float smooth = 0.1f;
-        float yAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, Rotation.eulerAngles.y, ref yVelocity, smooth);
-        transform.rotation = Quaternion.Euler(0, yAngle, 0); */
-        //transform.LookAt(Horizontal, Vertical);
-
         if (Dash)
             DashPower =  Dashing(HorizontalPower + VerticalPower);
 
         Controller.Move((HorizontalPower + Physics.gravity) * Force * Time.deltaTime);
         Controller.Move((VerticalPower + Physics.gravity) * Force * Time.deltaTime);
         
-        if (JumpCount <= MaxJumpCount)
+        if (JumpCount <= MaxJumpCount && Jump > 0.0f)
         {
             Controller.Move((JumpPower + Physics.gravity) * JumpForce * Time.deltaTime);
 
-            JumpCount = JumpCount + 1;
+            JumpCount++;
         }
         Controller.Move(DashPower);
 
@@ -88,7 +75,7 @@ public class Movement : MonoBehaviour
     {
         MoveHorizontal = Input.GetAxis("Horizontal");
         MoveVertical = Input.GetAxis("Vertical");
-        Jump = Input.GetAxis("Jump");
+        Jump = Input.GetKeyDown(KeyCode.Space) ? 1.0f : 0.0f;
         Sprint = Input.GetAxis("Sprint");
         Dash = Input.GetKeyUp(KeyCode.X);
     }
@@ -112,11 +99,13 @@ public class Movement : MonoBehaviour
         return DashTarget - gameObject.transform.position;
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            JumpCount = 0.0f;
+            Debug.Log($"JumpCount {JumpCount}");
+            JumpCount = 0;
+
         }
     }
 }
